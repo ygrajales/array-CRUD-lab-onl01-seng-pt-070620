f53d222461c0b51549673d49ccbda9bc53aec39c
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-10 00:18:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  hamilton_songs = ["Schuyler Sisters", "Satisfied", "Wait for It", "Helpless"]
 end
 
 def add_element_to_end_of_array(array, element)
